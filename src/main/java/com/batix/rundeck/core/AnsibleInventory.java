6b84f7d9cf0a097d5257af5c81a667691e0b65cc
ggwena <gwenael.lord@b-com.com>
2020-06-16 17:14:37 +0200

    squash: move to JsonElement

--- src/main/java/com/batix/rundeck/core/AnsibleInventory.java
+++ src/main/java/com/batix/rundeck/core/AnsibleInventory.java
@@ -12,24 +12,41 @@ public class AnsibleInventory {
 
   public class AnsibleInventoryHosts {
 
-    protected Map<String, JsonObject> hosts = new HashMap<String, JsonObject>();
+    protected Map<String, Map<String, JsonElement>> hosts = new HashMap<String, Map<String, JsonElement>>();
     protected Map<String, AnsibleInventoryHosts> children = new HashMap<String, AnsibleInventoryHosts>();
 
     public AnsibleInventoryHosts addHost(String nodeName) {
-      hosts.put(nodeName, new JsonObject());
+      hosts.put(nodeName, new HashMap<String, JsonElement>());
       return this;
     }
 
     public AnsibleInventoryHosts addHost(String nodeName, String host, Map<String, String> attributes) {
       attributes.put("ansible_host", host);
 
-      // convert back 'String attribute.value' to JsonArray or JsonObject, when not a Primitive
-      JsonObject attributesJson = new JsonObject();
+      // deserialize 'String attribute.value' to JsonArray or JsonElement, when not a Primitive
+      Map<String, JsonElement> attributesJson = new HashMap<String, JsonElement>();
+      
       for (String attribute : attributes.keySet()) {
         System.out.println("attributes: " + attribute + "="+ attributes.get(attribute));
+        JsonElement json = new JsonParser().parse(attributes.get(attribute));
+        System.out.println("attribute " + attribute + " is jsonObject? " + json.isJsonObject());
+        System.out.println("attribute " + attribute + " is isJsonArray? " + json.isJsonArray());
+        System.out.println("attribute " + attribute + " is isJsonNull? " + json.isJsonNull());
+        System.out.println("attribute " + attribute + " is isJsonPrimitive? " + json.isJsonPrimitive()); // OK!
+
+        // JsonObject attributeJson = json.getAsJsonObject();
+        // attributesJson.put(attribute, attributeJson);
+
+        attributes: bios_date=12/12/2018
+        Use JsonReader.setLenient(true) to accept malformed JSON
+
+        System.out.println("json: " + json);
+        attributesJson.put(attribute, json);
+
+
         // JsonElement attributeJson = new JsonParser().parse(attributes.get(attribute));
-        JsonElement attributeJson = new Gson().fromJson(attributes.get(attribute), JsonElement.class);
-        attributesJson.add(attribute, attributeJson);
+        // JsonElement attributeJson = new Gson().fromJson(attributes.get(attribute), );
+        // attributesJson.add(attribute, attributeJson);
       }
 
       hosts.put(nodeName, attributesJson);
